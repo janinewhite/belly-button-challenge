7c68946e8a7e4096c4cf5cded2a8fb5cf612f213
Janine White <janine.white@gmail.com>
2019-12-02 20:04:38 -0700

    Gauge working

--- assets/js/app.js
+++ assets/js/app.js
@@ -46,20 +46,47 @@ function getSample(samples,sample_id) {
     }
 }
 
+//Calculate gauge statistics
+var gauge_min = 100;
+var gauge_max = 0;
+var gauge_mean = 0;
+
+function getGaugeStats(metaData) {
+    let washing_frequency = 0;
+    let washing_sum = 0;
+    for (i = 0; i < metaData.length; i++) {
+        let meta = metaData[i].wfreq;
+        if (meta != null) {
+            washing_frequency += 1;
+            washing_sum += meta;
+            if (meta < gauge_min) {
+                gauge_min = meta;
+            }
+            if (meta > gauge_max) {
+                gauge_max = meta;
+            }
+        }
+    }
+    gauge_mean = washing_sum/washing_frequency;
+}
+
+//Display the demographics of a subject
 function updateDemographics(meta){
     console.log(meta);
+    let subjectid = "ID: "+meta.id;
     let subject_ethnicity = "Ethnicity: "+meta.ethnicity;
     let subject_gender = "Gender: "+meta.gender;
     let subject_age = "Age: "+meta.age;
     let subject_location = "Location: "+meta.location;
     let subject_bbtype = "Gender: "+meta.bbtype;
     let subject_washfreq = "Washing Frequency: "+meta.wfreq;
+
+    d3.select(".subjectid").text(subjectid);
     d3.select(".ethnicity").text(subject_ethnicity);
     d3.select(".gender").text(subject_gender);
     d3.select(".age").text(subject_age);
     d3.select(".location").text(subject_location);
     d3.select(".bbtype").text(subject_bbtype);
-    d3.select(".wash-freq").text(subject_washfreq);
 }
 
 // Plot data
@@ -94,12 +121,40 @@ function buildPlot(filter){
         showlegend: false
     };
     Plotly.newPlot("bubble",bubble_data,bubble_layout);
+    var washing_freq = meta.wfreq;
+    var gauge_data = [
+        {
+            domain: { x: [0, 1], y: [0, 1] },
+            value: washing_freq,
+            title: { text: "Washing Frequency" },
+            type: "indicator",
+            mode: "gauge+number",
+            gauge: {
+                axis: { range: [null, gauge_max]},
+                steps: [
+                    { range: [gauge_min, gauge_mean], color: "lightgray"},
+                    { range: [gauge_mean, gauge_max], color: "rgb(133,252,138)"}
+                ],
+                threshold: {
+                    line: { color: "blue", width: 4 },
+                    thickness: 0.75,
+                    value: gauge_mean
+                }
+            }
+        }
+    ];
+    var gauge_layout = {
+        height: 175,
+        margin: { t: 0, b: 0}
+    };
+    Plotly.newPlot('gauge', gauge_data, gauge_layout);
 }
 
 d3.json(data_url).then(data => {
     console.log(data);
     jsondata = data;
     defineData(jsondata);
+    getGaugeStats(metaData);
     makeSelect(subjects);
     var selectSubject = d3.select("#subject-selection")
     selectSubject.on("change", function(){
